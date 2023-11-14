6e54594a318eace04d50fe5f62b920f909ce9de9
Juan Villalobos <juanjosevillalobos.19@gmail.com>
2023-11-14 16:40:44 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/css/app.css' 
+import SideBar from './components/SideBar'
+function App (){
+//sideBar y contentWrapper
+  return (
+    <>
+      <SideBar />
+    </>
+  )
+  }
+
+export default App
